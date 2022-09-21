fc408bd0eeb3960258c97c09d7fa19bc2222a969
Sujaneeeee <113500965+Sujaneeeee@users.noreply.github.com>
2022-09-21 15:30:24 +0800

    0921

--- 121111.js
+++ 121111.js
@@ -1,5 +1,8 @@
 
 var readline = require('readline-sync')
 
-var height = readline.question("名")
-console.log("name"+height)
+var height = readline.question("身高")
+var weight = readline.question("體重")
+var bmi = (weight*weight)/[(height/10)*(height/10)]
+//console.log("你身高"+height + '\n,weight:'+weight)
+console.log("your bmi=" + bmi)
