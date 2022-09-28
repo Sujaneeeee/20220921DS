43165538d29649330e4f15796df5748f6a84fa8e
sujaneeeee <a0966415021@gmail.com>
2022-09-28 14:51:05 +0800

    0928

--- bmi.js
+++ bmi.js
@@ -1,8 +1,7 @@
-
 var readline = require('readline-sync')
 
 var height = readline.question("your height")
 var bmi = readline.question("your bmi")
 var weight=bmi*[(height/100)*(height/100)]
-console.log("你身高"+height + 'bmi'+bmi)
-console.log("your weight=" + weight)
+console.log("your height:"+height + 'your bmi:'+bmi)
+console.log("your weight:=" + weight)
