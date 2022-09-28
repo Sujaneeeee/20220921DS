60ee6ba5b2d2396c58409cf5a36c061aff914a7d
sujaneeeee <a0966415021@gmail.com>
2022-09-28 15:31:55 +0800

    0928

--- 121111.js
+++ 121111.js
@@ -1,8 +1,36 @@
+class BMI{
+    constructor(h, w){
+        
+        this.height = h;
+        this.weight = w;
+    }
 
-var readline = require('readline-sync')
+    bmi = function(){
+        var hPower2 = Math.pow(this.height / 100, 2);
+        var bmi = this.weight / hPower2;
+        return bmi;
+    }
+}
 
-var height = readline.question("your height")
-var bmi = readline.question("your bmi")
-var weight=bmi*[(height/100)*(height/100)]
-console.log("你身高"+height + 'bmi'+bmi)
-console.log("your weight=" + weight)
+var readline = require("readline-sync");
+for (; true; ) {   
+//do{
+//while (true) {
+    var height = readline.question("Ur height?");
+    if (isNaN(height) || height < 10 || height > 220) {
+        console.log("Please input ur height between 10 and 200");
+        continue;
+    }
+
+    var weight = readline.question("Ur weight?");
+    if (isNaN(weight) || weight < 10 || weight > 200) {
+        console.log("Please...");
+        continue;
+    }
+    break;
+}while(true);
+var mybmi = new BMI(height, weight);
+//var mybmi2 = new BMI(170, 60);
+
+console.log(mybmi.bmi())
+//console.log(mybmi2.bmi())
