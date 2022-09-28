0618564e777e1030e4f27c8cb7f2102be5d3606d
sujaneeeee <a0966415021@gmail.com>
2022-09-28 15:38:29 +0800

    0921 homework

--- bmi.js
+++ bmi.js
@@ -1,19 +1,7 @@
 var readline = require('readline-sync')
 
-while(ture) {
-    var height = readline.question("your height")
-      if(isNaN(height)|| height<10 || height>200){
-        console.log("please...")
-        continue
-      }
-      var weight = readline.question("your weight")
-if(isNaN(weight)|| weight<10 || weight>200){
-    console.log("please...")
-    continue
-
-    }
-}
-
-var bmi=weight/[(height/100)*(height/100)]
-console.log("your height:"+height + 'your weight:'+weight)
-console.log("your bmi:=" + bmi)
+var height = readline.question("your height")
+var bmi = readline.question("your bmi")
+var weight=bmi*[(height/100)*(height/100)]
+//console.log("your height"+height + 'bmi'+bmi)
+console.log("your weight=" + weight)
