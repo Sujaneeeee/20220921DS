99e2891fe8526d72783129c651de00ad5ce733cd
sujaneeeee <a0966415021@gmail.com>
2022-10-26 14:45:02 +0800

    1026

--- desk.js
+++ desk.js
@@ -24,7 +24,9 @@ let deck = [];
 // create a deck of cards
 for (let i = 0; i < suits.length; i++) {
     for (let x = 0; x < values.length; x++) {
-        let card = {Value: values[x], Suit: suits[i], File: values[x]+"_of_"+suits[i]+".png" };
+ 
+        var card = {Point: x<10 ? x+1 : 10, Value: values[x], Suit: suits[i], File: values[x]+"_of_"+suits[i]+".png" };
+         
         deck.push(card);
     }
 }
@@ -63,9 +65,53 @@ console.log('The first five cards are:');
 
 // display 5 results
 var TotalPoint = 0;
-for (let i = 0; i < 5; i++) {
-    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
-    document.getElementById("card"+i).src = "PNG-cards/"+deck[i].File;
-    TotalPoint += deck[i].Point;
+var count=0;
 
+var player1 = [], player2= [];
+
+// for (let i = 0; i < 2; i++) {
+//     console.log(`${deck[i].Value} of ${deck[i].Suit}`)
+//     var newcard = document.createElement("img");
+//     newcard.width = 250;
+//     newcard.src = "PNG-cards/"+deck[i].File;
+//     document.getElementById("Cards").appendChild(newcard)
+//     //document.getElementById("card"+i).src = "PNG-cards/"+deck[i].File;
+
+//     TotalPoint += deck[i].Point;
+//     count++;
+// }  
+
+player1.push(deck.pop());
+player1.push(deck.pop());
+player1.forEach(function(card){
+    var newcard = document.createElement("img");
+     newcard.width = 250;
+     newcard.src = "PNG-cards/"+card.File;
+     document.getElementById("Cards").appendChild(newcard)
+     TotalPoint += card.Point;
+})
+// for(var i=0; i<player1.length;i++){
+//     player1[i]
+// }
+
+document.getElementById("Point").innerHTML=TotalPoint;
+// function addcard(){
+//     var newcard = document.createElement("img");
+//     newcard.width = 250;
+//     newcard.src = "PNG-cards/"+deck[count].File;
+//     TotalPoint += deck[count].Point;
+//     document.getElementById("Cards").appendChild(newcard)
+//     document.getElementById("Point").innerHTML=TotalPoint;
+//     count++;
+// }
+
+function addcard(){
+        var newcard = document.createElement("img");
+        newcard.width = 250;
+        player1.push(deck.pop());
+        newcard.src = "PNG-cards/"+player1[player1.length-1].File;
+        TotalPoint += player1[player1.length-1].Point;
+        document.getElementById("Cards").appendChild(newcard)
+        document.getElementById("Point").innerHTML=TotalPoint;
+        count++;
 }
